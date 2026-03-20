93c4f1204ade701f6312143491009946d9020e6d
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 17:40:11 +0530

    feat: integrate webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,1 @@
+const path = require('path');module.exports = {  entry: './src/index.tsx',  output: {    path: path.resolve(__dirname, 'build'),    filename: 'index.js'  },  module: {    rules: [      {        test: /\.(ts|tsx)$/,        use: 'ts-loader',        exclude: /node_modules/      }    ]  },  resolve: {    extensions: ['.ts', '.tsx', '.js']  }};
