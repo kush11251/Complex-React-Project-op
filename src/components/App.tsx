2411743e88c9c97c5f2aee7516aeeb4e926e81e6
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 17:40:15 +0530

    feat: integrate src/components/App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,1 @@
+import React from 'react';import Header from './Header';import Footer from './Footer';import Body from './Body';const App = () => {  return (    <div>      <Header />      <Body />      <Footer />    </div>  );};export default App;
